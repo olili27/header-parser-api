76acb4d5e9b1fb76ab64aad831b23b7af7786736
Timothy Olili <timmolili27@gmail.com>
2022-07-06 18:00:15 +0300

    add a controller to get ipaddress, language and software

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.status(200).json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
 });
 
 
